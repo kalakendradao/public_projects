7047cb37f5a4a3281163bb61a997a0f946e41262
kalakendradao <109590450+kalakendradao@users.noreply.github.com>
2022-09-18 03:10:04 +0530

    Update setup_part3.sh

--- node_automation/setup_part3.sh
+++ node_automation/setup_part3.sh
@@ -1,3 +1,6 @@
+neardpid=$(pidof neard)
+echo $neardpid
+kill $neardpid
 cp /root/public_projects/node_automation/neard.service /etc/systemd/system/
 sudo systemctl enable neard
 sudo systemctl start neard
